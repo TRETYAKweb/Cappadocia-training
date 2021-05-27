b463055bfd11f7eda065ed72d05bd29da99b8c34
tretyakbro <tretyakbro@gmail.com>
2021-05-27 23:25:06 +0300

    Сделал блок Fairy-tail

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+
+  $('.fairy-tail__slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="./images/arrow-prev.svg" alt="arrow-prev"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="./images/arrow-next.svg" alt="arrow-next"></button>',
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true
+  });
+});
