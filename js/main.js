6ad9c5575c02c8a1ad4eb379b85a961dc09c27a2
tretyakbro <tretyakbro@gmail.com>
2021-05-28 04:41:43 +0300

    Сделал блок Our trip

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function () {
 
-  $('.fairy-tail__slider').slick({
+  $('.fairy-tail__slider, .our-trip__slider').slick({
     prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="./images/arrow-prev.svg" alt="arrow-prev"></button>',
     nextArrow: '<button type="button" class="slick-btn slick-next"><img src="./images/arrow-next.svg" alt="arrow-next"></button>',
     autoplay: true,
