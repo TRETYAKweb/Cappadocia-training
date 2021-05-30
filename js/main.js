e6c1e5f45fe2913c5fdf36ede606ed9ad8f296e6
tretyakbro <tretyakbro@gmail.com>
2021-05-30 18:47:24 +0300

    Сделал адаптив для блока Fairy tail

--- js/main.js
+++ js/main.js
@@ -5,6 +5,14 @@ $(function () {
     nextArrow: '<button type="button" class="slick-btn slick-next"><img src="./images/arrow-next.svg" alt="arrow-next"></button>',
     autoplay: true,
     autoplaySpeed: 3000,
-    fade: true
+    fade: true,
+    responsive: [
+      {
+        breakpoint: 601,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
   });
 });
